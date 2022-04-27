a85b93658f8b47db39c4cba2798b834c80807312
SecretStar112 <edwardxian062@gmail.com>
2022-04-27 04:58:43 +0300

    fix docstring

--- web_app/prediction.py
+++ web_app/prediction.py
@@ -5,10 +5,15 @@ from run_web_app import model
 
 
 def get_prediction(file):
-    """
+    """Make prediction
+
         Params:
         file: Uploaded image
+
+        Returns:
+        results: Predicted image
     """
+
     img_bytes = file.read()
     img = Image.open(io.BytesIO(img_bytes))
     results = model(img, size=640)
